a0934449a454e749552023e9b9a17c7098f6daf0
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2022-11-27 21:01:25 -0500

    Add card

--- src/pages/PrincipalPage.jsx
+++ src/pages/PrincipalPage.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
+import Card from "../components/CARDS/Card";
 import NavBarPrincipal from "../components/NavBar/NavBarPrincipal";
+import { getAllProducts } from "../service/ProductService";
 
 const PrincipalPage = () => {
+  const [products, setProducts] = useState([]);
+
+  const listAllProducts = async () => {
+    const result = await getAllProducts();
+    console.log(result.data.results);
+    setProducts(result.data.results);
+  };
+
+  useEffect(() => {
+    listAllProducts();
+    // console.log("HOLA EFFECT");
+    // console.log(products);
+  }, []);
+
   return (
     <>
       <NavBarPrincipal />
-      <h1>Hola Principal Page</h1>
+      <div className="container">
+        {products.map((product) => (
+          <Card
+            key={product._id}
+            id={product._id}
+            name={product.name}
+            description={product.description}
+            price={product.price}
+            image={product.image}
+          />
+        ))}
+      </div>
     </>
   );
 };
