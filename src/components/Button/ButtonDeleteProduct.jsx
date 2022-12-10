a872a240c8f5b642b651ac06ac2519a05e0ec156
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2022-12-10 18:39:52 -0500

    Add detail sale

--- src/components/Button/ButtonDeleteProduct.jsx
+++ src/components/Button/ButtonDeleteProduct.jsx
@@ -2,7 +2,7 @@ const ButtonDeleteProduct = () => {
 
   return (
     <>
-      <button>
+      <button className="button__cart product__button">
         <i className="bi bi-trash3-fill"></i> Eliminar
       </button>
     </>
